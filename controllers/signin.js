f045aae35e0a5564a03fdaf325d3eb858833b49f
Bruno Mariz <bruno.mariz@semantix.inc>
2022-02-13 12:20:48 -0300

    add form validation

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,15 +1,20 @@
 const handle_signin = (req, res, bcrypt, db) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("Incorrect form submission.");
+  }
+
   db("users")
     .select("email", "hash")
     .from("login")
-    .where("email", "=", req.body.email)
+    .where("email", "=", email)
     .then((data) => {
-      const is_valid = bcrypt.compareSync(req.body.password, data[0].hash);
+      const is_valid = bcrypt.compareSync(password, data[0].hash);
       if (is_valid) {
         return db
           .select("*")
           .from("users")
-          .where("email", "=", req.body.email)
+          .where("email", "=", email)
           .then((user) => {
             res.json(user[0]);
           })
